da0e2af810c3c03b8aff4501ef4ed10613daf56b
Cooper k <cooperkelley45@gmail.com>
2022-11-08 00:11:24 -0600

    final-formatting

--- Node.java
+++ Node.java
@@ -1,36 +1,57 @@
 // Cooper Kelley (clk200002)
 
-public class Node <T> {
-    private Node<T> next, down, prev;
-    private T payload;
+public class Node<T> {
 
-    Node() {
-        next = null;
-        down = null;
-        prev = null;
-        payload = null;
-    }
+  private Node<T> next, down, prev;
+  private T payload;
 
-    Node(T pl) {
-        payload = pl;
-    }
+  Node() {
+    next = null;
+    down = null;
+    prev = null;
+    payload = null;
+  }
 
-    Node(Node<T> n, Node<T> d, Node<T> pr, T pl) {
-        next = n;
-        down = d;
-        prev = pr;
-        payload = pl;
-    }
+  Node(T pl) {
+    payload = pl;
+  }
 
-    void setNext(Node<T> n) { next = n; }
-    void setDown(Node<T> d) { down = d; }
-    void setPrev(Node<T> pr) { prev = pr; }
-    void setPayload (T pl) { this.payload = pl; }
+  Node(Node<T> n, Node<T> d, Node<T> pr, T pl) {
+    next = n;
+    down = d;
+    prev = pr;
+    payload = pl;
+  }
 
-    Node<T> getNext() { return next; }
-    Node<T> getDown() { return down; }
-    Node<T> getPrev() { return prev; }
-    T getPayload() { return payload; }
-    
+  void setNext(Node<T> n) {
+    next = n;
+  }
 
+  void setDown(Node<T> d) {
+    down = d;
+  }
+
+  void setPrev(Node<T> pr) {
+    prev = pr;
+  }
+
+  void setPayload(T pl) {
+    this.payload = pl;
+  }
+
+  Node<T> getNext() {
+    return next;
+  }
+
+  Node<T> getDown() {
+    return down;
+  }
+
+  Node<T> getPrev() {
+    return prev;
+  }
+
+  T getPayload() {
+    return payload;
+  }
 }
--- Seat.java
+++ Seat.java
@@ -4,28 +4,42 @@ import java.util.*;
 
 public class Seat {
 
-    private int row;
-    private char seat, type;
+  private int row;
+  private char seat, type;
 
-    Seat() {
-        row = -1;
-        seat = '0';
-        type = '0';
-    }
+  Seat() {
+    row = -1;
+    seat = '0';
+    type = '0';
+  }
 
-    Seat(int r, char s, char t) {
-        row = r;
-        seat = s;
-        type = t;
-    }
+  Seat(int r, char s, char t) {
+    row = r;
+    seat = s;
+    type = t;
+  }
 
-    void setRow(int r) { row = r; }
-    void setSeat(char s) { seat = s; }
-    void setType(char t) { type = t; }
+  void setRow(int r) {
+    row = r;
+  }
 
-    int getRow() { return row; }
-    char getSeat() { return seat; }
-    char getType() { return type; }
+  void setSeat(char s) {
+    seat = s;
+  }
 
-    
+  void setType(char t) {
+    type = t;
+  }
+
+  int getRow() {
+    return row;
+  }
+
+  char getSeat() {
+    return seat;
+  }
+
+  char getType() {
+    return type;
+  }
 }
